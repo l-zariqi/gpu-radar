3acadc589c22ffa6b25b5d9d045ca9629cea2416
Lind <lind_zariqi@hotmail.com>
2025-03-06 00:54:48 +0000

    Updated API fetch

--- scripts/fetchWorker.js
+++ scripts/fetchWorker.js
@@ -7,7 +7,8 @@ self.addEventListener('message', (event) => {
         console.log('Fetching data for locale:', locale);
 
         // Fetch data from the API with the selected locale
-        fetch(`https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=${locale}`)
+        // fetch(`https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=${locale}&category=GPU`)
+        fetch(`https://api.nvidia.partners/edge/product/search?locale=${locale}&page=1&limit=12&manufacturer=NVIDIA&manufacturer_filter=NVIDIA~3,ASUS~32,GAINWARD~5,GIGABYTE~13,INNO3D~3,KFA2~1,MSI~21,PALIT~8,PNY~7,ZOTAC~10&category=GPU`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Network response was not OK: ${response.statusText}`);
